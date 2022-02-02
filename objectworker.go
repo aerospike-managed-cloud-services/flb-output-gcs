09ff7b5e27c9690501841a8c5eb8fb2ee0d2cf5a
Cory Dodt <cdodt@aerospike.com>
2022-02-02 01:24:36 +0000

    ams-10812 dumb streaming that writes to network every flush and rolls every 8 writes

--- objectworker.go
+++ objectworker.go
@@ -5,7 +5,7 @@ import (
 	"context"
 	"fmt"
 	"io"
-	"strings"
+	"log"
 	"time"
 
 	"cloud.google.com/go/storage"
@@ -13,25 +13,27 @@ import (
 
 // manages the lifetime of a gcs object
 type ObjectWorker struct {
-	tag         string
-	bucket_name string
-	object_path string
-	last        time.Time
-	written     int64
-	builder     strings.Builder
-	writer      *storage.Writer
+	bucket_name     string
+	last            time.Time
+	object_path     string
+	prefix          string
+	tag             string
+	writer          *storage.Writer
+	written         int64
+	PUT_COUNT_FIXME int
 }
 
 func NewObjectWorker(tag string, bucket_name, prefix string) *ObjectWorker {
 	last := time.Now()
 	object_path := fmt.Sprintf("%s/%s-%d", prefix, tag, last.Unix())
 	return &ObjectWorker{
-		tag:         tag,
-		bucket_name: bucket_name,
-		object_path: object_path,
-		last:        last,
-		written:     0,
-		builder:     strings.Builder{},
+		bucket_name:     bucket_name,
+		last:            last,
+		object_path:     object_path,
+		prefix:          prefix,
+		tag:             tag,
+		written:         0,
+		PUT_COUNT_FIXME: 0,
 	}
 }
 
@@ -39,22 +41,15 @@ func (work *ObjectWorker) FormatBucketPath() string {
 	return fmt.Sprintf("gs://%s/%s", work.bucket_name, work.object_path)
 }
 
-// FIXME - we'll remove this when streaming works
-// // upload log text to an object, clobbering whatever was there before
-// func (work *ObjectWorker) Clobber(client *storage.Client, buf bytes.Buffer) (int, error) {
-// 	ctx := context.Background()
-// 	wc := client.Bucket(work.bucket_name).Object(work.object_path).NewWriter(ctx)
-// 	n, err := wc.Write(buf.Bytes())
-// 	wc.Close()
-// 	return n, err
-// }
-
 // initialize a writer to write data to the object this worker manages
-func (work *ObjectWorker) beginStreaming(client *storage.Client) {
+func (work *ObjectWorker) beginStreaming(client *storage.Client) error {
 	ctx := context.Background()
 
-	work.writer = client.Bucket(work.bucket_name).Object(work.object_path).NewWriter(ctx)
+	work.writer = client.Bucket(work.bucket_name).Object(work.object_path).NewWriter(ctx) // TODO errors
+
 	work.writer.ChunkSize = 256 * 1024 // this is the smallest chunksize you can set and still have buffering
+
+	return nil
 }
 
 // write strings to a worker
@@ -69,14 +64,29 @@ func (work *ObjectWorker) Put(client *storage.Client, buf bytes.Buffer) error {
 
 	work.last = time.Now()
 
+	work.PUT_COUNT_FIXME += 1
+	log.Printf("~~ [%s].PUT_COUNT_FIXME = %d", work.FormatBucketPath(), work.PUT_COUNT_FIXME)
+
+	if work.PUT_COUNT_FIXME >= 8 {
+		work.roll(client)
+		work.PUT_COUNT_FIXME = 0
+	}
+
 	return nil
 }
 
 // roll over to the next gcs object name
-func (work *ObjectWorker) roll() error {
-	return nil
+func (work *ObjectWorker) roll(client *storage.Client) error {
+	prev := work.FormatBucketPath()
+	work.writer.Close()
+	work.last = time.Now()
+	work.object_path = fmt.Sprintf("%s/%s-%d", work.prefix, work.tag, work.last.Unix())
+	log.Printf("~~ [%s] rolls over => %s", prev, work.FormatBucketPath())
+
+	return work.beginStreaming(client)
 }
 
 func (work *ObjectWorker) Stop() error {
+	log.Printf("~~ [%s] Stop()", work.FormatBucketPath())
 	return work.writer.Close()
 }
